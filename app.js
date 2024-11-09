a7b62d8bce248cc54e3060e445a8d11637c05046
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-11-09 15:12:01 +0700

    Refactor to simplify the code

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ function getValue(node) {
     return node.textContent;
 }
 
+function setInputStatus(keyInput) {
+    if (keyInput === '=' || keyInput === 'Enter') {
+        inputStatus = '=';
+    } else {
+        inputStatus = 'i';
+    }
+}
+
 function getLastNum() {
     /**
      * get last number from display
@@ -20,106 +28,29 @@ function getLastNum() {
     return lastNum;
 }
 
-function validateInput(input) {
-    /**  
-     * It will check if the input is valid 
-     * and will return the valid displayedValue
-     */
-
-    const displayedValue = getValue(displayNode);
-    
-    // if the input is a number then always valid
-    if (!isNaN(input)) {
-        if (displayedValue === '0' || inputStatus === '=') return input;
-        return displayedValue + input;
-    } 
-
-    input = input === '*' ? 'x' : input;
-    if (input === 'x' || input === '-' || input === '+' || input === '/') {
-        const lastInput = displayedValue.slice(-1);
-        // if the last input is not a number then not valid and will be replaced
-        if (isNaN(lastInput)) {
-            return displayedValue.slice(0, -1) + input;  
-        } 
-    }
-
-    if (input === '.') {
-        const lastNum = getLastNum();
-        if (inputStatus === '=') return "0."
-        if (lastNum.indexOf('.') >= 0) return displayedValue;
-    }
-
-    return displayedValue + input;
-}
-
 function backspace() {
-    // will return string display after backspace
+    // will return string display that's been sliced by 1 character
     let displayValue = getValue(displayNode)
     if (displayValue.length === 1) return '0';
 
     return displayValue.slice(0,-1);
 }
 
-function display() {
-    /**
-     * To display inputs to screen
-     */
-    const value = getValue(this);
-    const validInput = validateInput(value);
-
-    displayNode.textContent = validInput;
-    inputStatus = 'i';
-}
-
-function calc() {
-    /**
-     * To do calculation when '=' is clicked
-     */
-    let value = getValue(displayNode);
-    // if the last char in displayed value is not a number the it will be removed
-    const lastChar = value.slice(-1);
-    if (isNaN(lastChar)) {
-        value = value.slice(0, -1);
-    } 
-    
-    let result = eval(value.replace('x', '*'));
-    displayNode.textContent = result;
-    inputStatus = '=';
-    return result;
-}
-
-/**
- * Add event listener
- */
-for (let i=0; i<btnNums.length; i++) {
-    btnNums[i].addEventListener('click', display);
-}
-
-for (let i=0; i<btnOperators.length; i++) {
-    btnOperators[i].addEventListener('click', display);
-}
-
-document.getElementById('equal').addEventListener('click', calc);
-document.getElementById('ac').addEventListener('click', () => {
-    displayNode.textContent = 0;
-});
-document.getElementById('backspace').addEventListener('click', () => {
-    displayNode.textContent = backspace();
-})
-
-
-// Keyboard input
-
 function validateKey(key) {
     /**  
-     * It will check if the keyboard input is valid 
+     * It will check if the input is valid 
      * and will return the valid displayedValue
      */
 
     const displayedValue = getValue(displayNode);
 
     // only allowed key can proceed
+    // "ye shall not pass if ye not qualified!" (No need to worry Gandalf will oversee this function)
     if (!(!isNaN(key) || key==='+' || key==='-' || key==='/' || key==='x' || key==='*' || key==='Backspace' || key==='.' || key==='Enter' || key==='=')) return displayedValue; 
+    
+    if (key === '=' || key === 'Enter') {
+        return calc();
+    }
     
     // if the input is a number then always valid
     if (!isNaN(key)) {
@@ -142,10 +73,6 @@ function validateKey(key) {
         if (lastNum.indexOf('.') >= 0) return displayedValue;
     }
 
-    if (key === '=' || key === 'Enter') {
-        return calc();
-    } 
-
     if (key === 'Backspace') {
         return backspace();
     }
@@ -153,14 +80,65 @@ function validateKey(key) {
     return displayedValue + key;
 }
 
-// to display keboard input to screen
+function calc() {
+    /**
+     * To do calculation when '=' is clicked
+     */
+    let value = getValue(displayNode);
+    // validation
+    // if the last char in displayed value is not a number then it will be removed
+    const lastChar = value.slice(-1);
+    if (isNaN(lastChar)) {
+        value = value.slice(0, -1);
+    } 
+    
+    let result = eval(value.replace('x', '*'));
+    displayNode.textContent = result;
+    return result;
+}
+
+function inputButton() {
+    /**
+     * To display button inputs to screen
+     */
+    const keyInput = getValue(this);
+    const validInput = validateKey(keyInput);
+
+    displayNode.textContent = validInput;
+    setInputStatus(keyInput);
+}
+
 const inputKey = (e) => {
+    /**
+     * to display keyboard input to screen
+     */
     let validValue = validateKey(e.key);
 
     displayNode.textContent = validValue;
-    if (!(e.key === '=' || e.key === 'Enter')) {
-        inputStatus = 'i';
-    }
+    setInputStatus(e.key);
 }
 
+
+
+// EventListener for keyboard input
 document.addEventListener('keyup', inputKey);
+
+// EventListener for button input
+// numbers
+for (let i=0; i<btnNums.length; i++) {
+    btnNums[i].addEventListener('click', inputButton);
+}
+// operators
+for (let i=0; i<btnOperators.length; i++) {
+    btnOperators[i].addEventListener('click', inputButton);
+}
+// equal
+document.getElementById('equal').addEventListener('click', inputButton);
+// AC or reset
+document.getElementById('ac').addEventListener('click', () => {
+    displayNode.textContent = 0;
+});
+// backspace button
+document.getElementById('backspace').addEventListener('click', () => {
+    displayNode.textContent = backspace();
+})
